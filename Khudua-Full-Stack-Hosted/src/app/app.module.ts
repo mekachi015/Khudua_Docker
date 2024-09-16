450d63c798b73afb8b630119673a550fa4554aff
mekachi015 <141609950+mekachi015@users.noreply.github.com>
2024-09-16 14:08:56 +0200

    file-initiated

--- Khudua-Full-Stack-Hosted/src/app/app.module.ts
+++ Khudua-Full-Stack-Hosted/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { ServicesPageComponent } from './pages/services-page/services-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ProductsPageComponent,
+    ServicesPageComponent,
+    ContactPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
