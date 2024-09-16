a2714692985392fa10eb6517d197c3af94ae4d7a
mekachi015 <141609950+mekachi015@users.noreply.github.com>
2024-09-16 15:45:07 +0200

    navbar-implemented

--- Khudua-Full-Stack-Hosted/src/app/app-routing.module.ts
+++ Khudua-Full-Stack-Hosted/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { ServicesPageComponent } from './pages/services-page/services-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomePageComponent},
+  {path: 'home', component:HomePageComponent},
+  {path: 'about',component:AboutPageComponent},
+  {path: 'products', component:ProductsPageComponent},
+  {path: 'services', component:ServicesPageComponent},
+  {path: 'contact', component:ContactPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Khudua-Full-Stack-Hosted/src/app/components/nav-bar/nav-bar.component.ts
+++ Khudua-Full-Stack-Hosted/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+
+  isMenuOpen = false;
+
+  constructor (private router: Router) {}
+
+  //method to navigate to home page
+  generalRedirect(): void{
+    this.router.navigate(['/']);
+  }
+
+  // Method to navigate to the About Us page
+  goToHome(): void {
+    this.router.navigate(['home']);
+  }
+
+   // Method to navigate to the About Us page
+   goToAboutUs(): void {
+    this.router.navigate(['about']);
+  }
+
+  // Method to navigate to the Products page
+  goToProducts(): void {
+    this.router.navigate(['products']);
+  }
+
+  // Method to navigate to the Services page
+  goToServices(): void {
+    this.router.navigate(['services']);
+  }
+
+  // Method to navigate to the Find Us page
+  goToContact(): void {
+    this.router.navigate(['contact']);
+  }
+
+  // Method to toggle the mobile menu
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+
 
 }
